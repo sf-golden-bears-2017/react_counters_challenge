706e14bf6c5d76e328d65dd901dc7c2d9e710f1a
Caroline & Iris Nevins <pair+carolyoon.cosmosiris@devbootcamp.com>
2017-11-06 12:14:54 -0800

    Create total counter

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ class App extends React.Component {
     this.state = {
       count1:0,
       count2:0,
-      count3:0,
-      total: this.count1 + this.count2 + this.count3
+      count3:0
     }
 
     this.increment = this.increment.bind(this)
@@ -24,12 +23,17 @@ class App extends React.Component {
     this.setState({ [count]: this.state[count] - number })
   }
 
+  total(){
+    return this.state.count1 + this.state.count2 + this.state.count3
+  }
+
   render() {
     return (
       <div className="page-center-frame">
         <Counter myProp={this.state.count1} myProp3={() => this.increment("count1",1)} myProp4={() => this.decrement("count1", 1)}/>
         <Counter myProp={this.state.count2} myProp3={() => this.increment("count2",2)} myProp4={() => this.decrement("count2", 2)}  />
         <Counter myProp={this.state.count3} myProp3={() => this.increment("count3",3)} myProp4={() => this.decrement("count3", 3)} />
+        <h1>Total: {this.total()}</h1>
       </div>
     );
   }
