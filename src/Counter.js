b1671131c8c8e03a4e57864f2f4db557c26fd698
Caroline & Iris Nevins <pair+carolyoon.cosmosiris@devbootcamp.com>
2017-11-06 10:53:01 -0800

    Add decrement buttons

--- src/Counter.js
+++ src/Counter.js
@@ -9,10 +9,15 @@ class Counter extends React.Component {
     };
 
     this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
   }
 
   increment() {
     this.setState({ count: this.state.count + 1 })
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 })
   }
 
   render() {
@@ -20,6 +25,7 @@ class Counter extends React.Component {
       <div className="counter">
         <button onClick={this.increment}>+</button>
         <div className="count">{this.state.count}</div>
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
