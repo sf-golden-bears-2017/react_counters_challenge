b7cea2a80e17eadae4114ae7402200dd574eea68
Caroline <email": null>
2017-11-06 17:49:30 -0800

    Create form to add counter with user input

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ class App extends React.Component {
     super();
     this.state = {
       counters: [{count:0, changeBy: 1}, {count:0, changeBy: 2}, {count:0, changeBy: 3} ],
-      renderChild: true,
+      newChangeBy: 1
     }
 
     this.increment = this.increment.bind(this)
     this.decrement = this.decrement.bind(this)
     this.removeCounter = this.removeCounter.bind(this)
+    this.addCounter = this.addCounter.bind(this)
     this.total = this.total.bind(this)
   }
 
@@ -34,22 +35,40 @@ class App extends React.Component {
     this.setState({ counters: oldCounterArray })
   }
 
+  addCounter(e) {
+    e.preventDefault()
+    const oldCounterArray = this.state.counters
+    oldCounterArray.push({count: 0, changeBy: this.state.newChangeBy})
+    this.setState({ counters: oldCounterArray })
+  }
+
   total(){
     return this.state.counters.reduce(function(total, counter) {
       return total + counter.count;
     }, 0)
   }
 
+
+  // $('input').on('change', this.setValue)
+
+  // setValue(event) {
+  //   event.preventDefault()
+
+  //   $('input').value = event.target.value
+  // }
+
   render() {
     return (
-      // <ul>
       <div className="page-center-frame">
         {this.state.counters.map((counter, index) =>
             <Counter counter={ counter } increment={() => this.increment(index)} decrement={() => this.decrement(index)}  unmountMe={() => this.removeCounter(index)} />
           )}
           <h1>Total: {this.total()}</h1>
+          <form onSubmit={this.addCounter}>
+            <input type="number" onChange={(e) => this.setState({newChangeBy: parseInt(e.target.value)})} value={this.state.newChangeBy} />
+            <input type="submit" value="Create New Counter"/>
+          </form>
       </div>
-      // </ul>
     );
   }
 }
