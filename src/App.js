95d2a13cbfe8d19947cf4a6a74f6a131ec081bed
Quynh Nguyen <qnguyenx3@gmail.com>
2017-11-06 16:50:33 -0800

    Add a button to add a counter

--- src/App.js
+++ src/App.js
@@ -6,38 +6,61 @@ class App extends React.Component {
 constructor() {
   super();
   this.state = {
-    count1: 0,
-    count2: 0,
-    count3: 0
+    counters: [
+      {count: 0, multiplier: 1},
+      {count: 0, multiplier: 2},
+      {count: 0, multiplier: 3}
+    ]
   };
 
   this.increment = this.increment.bind(this)
   this.decrement = this.decrement.bind(this)
   this.total = this.total.bind(this)
+  this.addCounterButton = this.addCounterButton.bind(this)
 }
 
-  increment(counter, multiplier) {
-    this.setState({ [counter]: this.state[counter] + multiplier })
+  increment(index) {
+    const counters = this.state.counters
+    counters[index].count += counters[index].multiplier
+    this.setState({counters})
   }
 
-  decrement(counter, multiplier) {
-    this.setState({ [counter]: this.state[counter] - multiplier })
+  decrement(index) {
+    const counters = this.state.counters
+    counters[index].count -= counters[index].multiplier
+    this.setState({counters})
   }
 
   total() {
-    return this.state.count1 + this.state.count2 + this.state.count3
+    return this.state.counters.reduce((sum, counter) => sum + counter.count, 0);
+  }
+
+  addCounterButton() {
+    const counters = this.state.counters
+    counters.push({count: 0, multiplier: (this.state.counters.length + 1)})
+    this.setState({counters})
   }
 
   render() {
     return (
-      <div className="page-center-frame">
-        {(Object
-          .keys(this.state))
-          .map((counter, index) =>
-            <Counter multiplier={index + 1} increment={() => this.increment(counter, index + 1)} decrement={() => this.decrement(counter, index + 1)} count={this.state[counter]} />
-        )}
-        <div className="total">
-          <span>Total: {this.total()}</span>
+      <div className="parent-container">
+        <div className="page-center-frame">
+          {(this.state.counters)
+            .map((counter, index) =>
+              <Counter increment={() => this.increment(index)} decrement={() => this.decrement(index)} count={counter.count} />
+          )}
+
+          { /* <Counter multiplier={1} increment={() => this.increment("count1", 1)} decrement={() => this.decrement("count1", 1)} count={this.state.count1} />
+          <Counter multiplier={2} increment={() => this.increment("count2", 2)} decrement={() => this.decrement("count2", 2)} count={this.state.count2} />
+          <Counter multiplier={3} increment={() => this.increment("count3", 3)} decrement={() => this.decrement("count3", 3)} count={this.state.count3} /> */ }
+
+          <div className="total">
+            <span>Total: {this.total()}</span>
+          </div>
+
+          <div className="add-button">
+            <button onClick={this.addCounterButton}>+ counter</button>
+          </div>
         </div>
       </div>
     );
